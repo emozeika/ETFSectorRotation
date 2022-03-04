79f0a0663e92053f923672e0a256964d02aba9d6
Eric Mozeika <mozeikaeric@gmail.com>
2022-03-03 22:34:51 -0500

    started creating extractation class using Alpha Vantage API

--- extract_data.py
+++ extract_data.py
@@ -1,3 +1,27 @@
 import yfinance as yf 
 
 
+apiKey = 'ASWAK34G8POG9LJS'
+
+
+baseURL = 'https://www.alphavantage.co/query?'
+
+
+class AlphaVantageExtract:
+    baseURL = 'https://www.alphavantage.co/query?'
+
+    functionMapping = {
+        'intraday' : 'TIME_SERIES_INTRADAY',
+        'daily' : 'TIME_SERIES_DAILY',
+        'weekly' : 'TIME_SERIES_WEEKLY',
+        'monthly' : 'TIME_SERIES_MONTHLY'
+    }
+
+    def __init__(self, apiKey):
+        self.apiKey = apiKey
+        self.queryURL = ''
+
+    def create_query_url(self, timeseriestype, ticker, outputsize):
+        self.queryURL = self.baseURL + f'function={timeseriestype}&symbol={ticker}apikey={self.apiKey}'
+        print(self.queryURL)    
+AlphaVantageExtract('ERIC').create_query_url('daily', 'TSLA', 'full')
