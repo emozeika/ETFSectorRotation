472a173e0811a5b53ed0144997a3cb025feebe6f
Eric Mozeika <mozeikaeric@gmail.com>
2022-03-04 01:21:14 -0500

    added functions to request and parse kline data

--- av_extract.py
+++ av_extract.py
@@ -1,3 +1,5 @@
+import requests
+import pandas as pd
 
 
 class AlphaVantageExtract:
@@ -13,9 +15,55 @@ class AlphaVantageExtract:
     def __init__(self, apiKey):
         self.apiKey = apiKey
         self.queryURL = ''
+        self.data = None
 
     def create_query_url(self, timeseriestype, ticker, outputsize):
-        self.queryURL = self.baseURL + f'function={timeseriestype}&symbol={ticker}apikey={self.apiKey}'
-        print(self.queryURL)    
+        ''' 
+        Formats the query url string for KLINE data given the parameters. 
+        '''
+
+        self.queryURL = self.baseURL + f'function={self.functionMapping.get(timeseriestype)}&symbol={ticker}&apikey={self.apiKey}'
+
+
+        
+
+    def request_kline_data(self, timeseriestype, ticker, outputsize):
+        '''
+        Requests kline data in json and returns the json response
+        '''
+        self.create_query_url(timeseriestype, ticker, outputsize)
+        self.data = requests.get(self.queryURL).json()
+        
+
+
+    def parse_kline_data(self, timeseriestype, ticker, outputsize):
+        '''
+        Parses the OHLCV data out of the json response and stores into pandas dataframe
+        '''
+        data = []
+
+        self.request_kline_data(timeseriestype, ticker, outputsize)       
+        for key in self.data.keys():
+            if key != 'Meta Data':
+                for date in self.data[key].keys():
+                    curr_date = [date]
+                    for price, value in self.data[key][date].items():
+                        curr_date.append(value)
+                    data.append(curr_date)
+        
+        data = pd.DataFrame(data, columns= ['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+        return data
+                   
+
+
+
+
+
+
+    def request_data(self, timeseriestype, ticker, outputsize):
+        ''' 
+        Wrapper function to handle different data requests. KLINE, Fundemental, Indicators, etc...
+
+        '''
 
 
